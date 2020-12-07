278c2f015cc97be881a54f5cc6d3e53489ef6cbd
somaog <ikotcsei@gmail.com>
2020-12-07 21:19:07 +0100

    save

--- src/main/java/com/explorespring/exploreSP/auth/UnderConstruction/MyUserDetailsService.java
+++ src/main/java/com/explorespring/exploreSP/auth/UnderConstruction/MyUserDetailsService.java
@@ -10,19 +10,35 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/* The userdetailsservice return UserDetails object that contains user information
+	for authentication
+
+	UserDetailsService
+		gets user data from datasource (db, file, hardcoded, etc), returns as a UserDetails obj.
+
+	UserDetails
+	 https://docs.spring.io/spring-security/site/docs/3.0.x/apidocs/org/springframework/security/core/userdetails/UserDetails.html
+*
+* */
+
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	UserRepository userRepository;
 
+	//get user details obj via username
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		//optional may not have a value
+		//optional may not have a value, get user object via UserRepository
 		Optional<User> user = userRepository.findByUserName(userName);
 
 		//throwing an exception if user is empty
 		user.orElseThrow(() -> new UsernameNotFoundException("Not found : " + userName));
+
+		//return the UserDetails object created by user object
+		// map(function).get() - maps the function to the object, and gets the value
 		return user.map(MyUserDetails::new).get();
 	}
 
--- src/main/java/com/explorespring/exploreSP/auth/model/UserRepository.java
+++ src/main/java/com/explorespring/exploreSP/auth/model/UserRepository.java
@@ -3,7 +3,9 @@ package com.explorespring.exploreSP.auth.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+                                            // JpaRepository<return type, id-type>
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //automatic method generated findById, findBy.... (User object field vars)
     Optional<User> findByUserName(String userName);
+    Optional<User> findById(Integer id);
 }
